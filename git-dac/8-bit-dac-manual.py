b630b1f605050d41abb16ef86580c9efd46803c4
Pavel Artemyev <paul.artemyev@outlook.com>
2025-10-01 17:23:56 +0300

    dac

---
+++ git-dac/8-bit-dac-manual.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+
+dac_bits = [16, 20, 21, 25, 26, 17, 27, 22]
+
+GPIO.setmode(GPIO.BCM)
+
+for pin in dac_bits:
+    GPIO.setup(pin, GPIO.OUT)
+
+dynamic_range = 3.3
+
+def voltage_to_number(voltage):
+    if not (0.0 <= voltage <= dynamic_range):
+        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В")
+        return 0
+    
+    return int(voltage / dynamic_range * 255)
+
+def number_to_dac(number):
+    binary = [int(element) for element in bin(number)[2:].zfill(8)]
+    GPIO.output(dac_bits, binary)
+
+try:
+    while True:
+        try:
+            voltage = float(input("Ведите напряжение в Вольтах: "))
+            number = voltage_to_number(voltage)
+            number_to_dac(number)
+        
+        except ValueError:
+            print("Вы ввели не число. Попробуйте ещё раз\n")
+
+finally:
+    GPIO.output(dac_bits, 0)
+    GPIO.cleanup()
