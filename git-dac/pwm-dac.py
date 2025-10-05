a422644a941f79b1a0f440f833ca90ab4ca39c4e
Paul Artemyev <paul.artemyev@outlook.com>
2025-10-05 16:17:50 +0300

    git-dac

--- git-dac/pwm-dac.py
+++ git-dac/pwm-dac.py
@@ -1,41 +1,35 @@
 import RPi.GPIO as GPIO
 
-class R2R_DAC:
-    def __init__(self, gpio_bits, dynamic_range, verbose = False):
-        self.gpio_bits = gpio_bits
+class PWM_DAC:
+    def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose = False):
+        self.gpio_pin = gpio_pin
+        self.pwm_frequency = pwm_frequency
         self.dynamic_range = dynamic_range
         self.verbose = verbose
-
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
 
     def deinit(self):
         GPIO.output(self.gpio_bits, 0)
         GPIO.cleanup()
-        
+    
     def set_voltage(self, voltage):
         if not (0.0 <= voltage <= self.dynamic_range):
             print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В")
             return 0
             
         return int(voltage / self.dynamic_range * 255)
-
-    def set_number(self, number):
-        binary = [int(element) for element in bin(number)[2:].zfill(8)]
-        GPIO.output(self.gpio_bits, binary)
+    
 
 if __name__ == "__main__":
     try:
-        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.05, True)
-
+        dac = PWM_DAC(12, 500, 3.290, True)
+        
         while True:
             try:
                 voltage = float(input("Введите напряжение в Вольтах: "))
-                number = dac.set_voltage(voltage)  # Получаем число
-                dac.set_number(number)             # Устанавливаем на ЦАП
-            
+                dac.set_voltage(voltage)
+
             except ValueError:
                 print("Вы ввели не число. Попробуйте ещё раз\n")
-    
+
     finally:
         dac.deinit()
