5502f5acc450f5fa10bbabd82b052f8f58419a85
Paul Artemyev <paul.artemyev@outlook.com>
2025-11-10 21:27:05 +0300

    adc

--- get-adc/r2r_sc.py
+++ get-adc/r2r_sc.py
@@ -1,6 +1,6 @@
 import time
 from r2r_adc import R2R_ADC
-from adc_plot import plot_voltage_vs_time
+import adc_plot
 
 adc = R2R_ADC(dynamic_range=3.13, compare_time=0.01, verbose = False)
 
@@ -19,6 +19,8 @@ try:
         time_values.append(time.time()-start_time)
         time.sleep(0.05)
 
-    plot_voltage_vs_time(time_values, voltage_values, adc.dynamic_range)
+    adc_plot.plot_voltage_vs_time(time_values, voltage_values, adc.dynamic_range)
+    adc_plot.plot_sampling_period_hist(time_values)
+
 finally: 
     adc.cleanup()
